e228c038e5f984fb4c6de36c1eeca3502458525a
Amit998 <damitlucky998@gmail.com>
2020-09-25 10:50:39 +0530

    sign in

--- e_commerce_ui/lib/screens/sign_in/components/body.dart
+++ e_commerce_ui/lib/screens/sign_in/components/body.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce_ui/components/default_button.dart';
 import 'package:e_commerce_ui/constants.dart';
 import 'package:e_commerce_ui/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../../../components/custom_surfix_icon.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -40,26 +42,94 @@ class SignForm extends StatefulWidget {
 }
 
 class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = ["lol", "lol", "lolo"];
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Email",
-              hintText: "Enter Your Name",
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: SvgPicture.asset("assets/icons/Mail.svg")
-              // contentPadding: EdgeInsets.symmetric(horizontal: 50,vertical: 20),
-
-              
-              
-
-            ),
-          ),
+          buildEmailFormField(),
+          SizedBox(height: getProportionScreenHeight(20)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionScreenHeight(20)),
+          FormError(errors: errors),
+          DefaultButton(
+            text: "continue",
+            press: () {},
+          )
         ],
       ),
     );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter Your Password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+        // contentPadding: EdgeInsets.symmetric(horizontal: 50,vertical: 20),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value.isEmpty) {
+          setState(() {
+            errors.add("Please Enter Your Email");
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "Enter Your Name",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: "assets/icons/Mail.svg",
+        ),
+        // contentPadding: EdgeInsets.symmetric(horizontal: 50,vertical: 20),
+      ),
+    );
+  }
+}
+
+class FormError extends StatelessWidget {
+  const FormError({
+    Key key, this.errors,
+  }) : super(key: key);
+  final List<String> errors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: List.generate(errors.length, (index) => formErrorText(error: errors[index])),
+    );
+  }
+
+  Row formErrorText({ String error }) {
+    return Row(
+        children: [
+          SvgPicture.asset(
+            "assets/icons/Error.svg",
+            height: getProportionScreenHeight(24),
+            width: getProportionScreenWidth(24),
+          ),
+          SizedBox(
+            width: getProportionScreenWidth(10),
+          ),
+          Text(error),
+        ],
+      );
   }
 }
