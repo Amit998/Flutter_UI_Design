131b83429364620b9fbe60c3aabf0f8a9c93d784
Amit998 <damitlucky998@gmail.com>
2020-10-30 09:33:33 +0530

    food app

---
+++ food_ordering/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering/constants.dart';
+import 'package:food_ordering/screens/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme(
+          body1: TextStyle(color: ksecondaryColor),
+          body2: TextStyle(color: ksecondaryColor),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ food_ordering/lib/screens/home/home_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("asset/icons/menu.svg"),
+        ),
+      ),
+    );
+  }
+}
