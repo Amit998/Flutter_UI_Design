178cd129905c56f8097b736a2734a8caa522faec
Amit998 <damitlucky998@gmail.com>
2020-11-11 10:58:04 +0530

    yaari app

--- social_networking_site/android/app/src/main/kotlin/com/example/social_networking_site/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.social_networking_site
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ yaari_app/android/app/src/main/kotlin/com/example/yaari_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yaari_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
