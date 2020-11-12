75656a41f6ee3636102a402d5b97a9425b630101
Amit998 <damitlucky998@gmail.com>
2020-11-12 11:23:17 +0530

    firebase connected

--- yaari_app/lib/main.dart
+++ yaari_app/lib/main.dart
@@ -3,16 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("AMit"),
+          title: Text("AppBar"),
         ),
       ),
-      
     );
   }
 }
+
+// keytool -list -v -keystore "%USERPROFILE%\.android\debug.keystore"-alias androiddebugkey -storepass android -keypass android
+// keytool -list -v \
+// -alias androiddebugkey -keystore %USERPROFILE%\.android\debug.keystore
