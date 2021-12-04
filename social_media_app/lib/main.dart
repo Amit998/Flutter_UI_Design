fcfad9bd725866c8f4cdb7ede156c99b3511f900
amit998 <damitlucky998@gmail.com>
2021-12-04 12:59:41 +0530

    added

---
+++ social_media_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/pages/rootApp.dart';
+
+void main() {
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
